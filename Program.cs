7c1a5a9996e8adb1dac7f55d30a8c192152fcf65
Kirill1018 <115730059+Kirill1018@users.noreply.github.com>
2023-02-03 19:14:50 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cs
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            do
+            {
+                Console.WriteLine("выбор действия - сложение=0, вычитание=1, умножение=2, деление=3");
+                string action = Console.ReadLine();
+                switch (action)
+                {
+                    case "0":
+                        Console.Write("ввод первого значения=");
+                        double value_1 = Convert.ToDouble(Console.ReadLine());
+                        Console.Write("ввод второго значения=");
+                        double value_2 = Convert.ToDouble(Console.ReadLine());
+                        double result = value_1 + value_2;
+                        Console.WriteLine(result);
+                        break;
+                    case "1":
+                        Console.Write("ввод первого значения=");
+                        value_1 = Convert.ToDouble(Console.ReadLine());
+                        Console.Write("ввод второго значения=");
+                        value_2 = Convert.ToDouble(Console.ReadLine());
+                        result = value_1 - value_2;
+                        Console.WriteLine(result);
+                        break;
+                    case "2":
+                        Console.Write("ввод первого значения=");
+                        value_1 = Convert.ToDouble(Console.ReadLine());
+                        Console.Write("ввод второго значения=");
+                        value_2 = Convert.ToDouble(Console.ReadLine());
+                        result = value_1 * value_2;
+                        Console.WriteLine(result);
+                        break;
+                    case "3":
+                        Console.Write("ввод первого значения=");
+                        value_1 = Convert.ToDouble(Console.ReadLine());
+                        Console.Write("ввод второго значения=");
+                        value_2 = Convert.ToDouble(Console.ReadLine());
+                        if (value_2 == 0) Console.WriteLine("ошибка");
+                        result = value_1 / value_2;
+                        Console.WriteLine(result);
+                        break;
+                    default:
+                        Console.WriteLine("ошибка");
+                        break;
+                }
+            } while (true);
+        }
+    }
+}
